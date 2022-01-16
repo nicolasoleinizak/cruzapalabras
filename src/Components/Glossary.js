346879514a3c526072133b9c2b8928e8d5d9f455
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-01-16 12:49:21 -0300

    add functionallity (definition filling)
    add methods and fields to fill the words definition, add condition to show or not the words

--- src/Components/Glossary.js
+++ src/Components/Glossary.js
@@ -3,19 +3,46 @@ import React from 'react'
 class Glossary extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            glossary: []
+        }
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(e, index){
+        let modifiedEntry = {
+            ...this.state.glossary[index],
+            definition: e.target.value
+        }
+        this.setState(prevState => {
+            return{
+                glossary: prevState.glossary.slice(0, index).concat(modifiedEntry).concat(prevState.glossary.slice(index+1))
+            }
+        })
+    }
+
+    initialize(){
+        this.setState({
+            glossary: this.props.glossary
+        })
+    }
+
+    componentDidMount(){
+        this.initialize()
     }
 
     render(){
         return(
             <div className="container p-1">
-                <h3>{this.props.glossaryType}</h3>
+                <h3>{this.props.wordsDirection}</h3>
                 <div className="glossary-container">
                 {
-                    this.props.glossary.map(item => {
+                    this.state.glossary.map((item, index) => {
                         return(
-                            <div className="glossary-row">
-                                <span className="glossary-cell">{item.number}</span>
-                                <span className="glossary-cell">{item.word}</span>
+                            <div className={this.props.areGlossaryWordsVisible? 'glossary-full-row':'glossary-row'} key={"entry-"+index}>
+                                <span className="glossary-cell" style={{gridArea: 'number'}}>{item.number}</span>
+                                {this.props.areGlossaryWordsVisible && <span className="glossary-cell" style={{gridArea: 'word'}}>{item.word}</span>}
+                                <input type="text" className="form-control glossary-cell"  style={{gridArea: 'definition'}}placeholder="Definición..." onChange={(event) => this.handleInputChange(event, index)} value={item.definition}/>
                             </div>
                         )
                     })
